4b3a8717583fe3a85011f87f6c007bee84bfd35d
JANARTHANANPALANIVEL <janajana162004@gmail.com>
2025-04-12 01:36:58 +0530

    nil

--- services/test_generator.py
+++ services/test_generator.py
@@ -11,14 +11,16 @@ Requirements:
 1. Check page load and all functionality by analyzing the website
 2. Highlight each element tested with a 1px red border.
 3. Wait 0.8 seconds after interacting with each element.
-4. Scroll the page to ensure all elements are tested.
-5. check all the pages and elements fucntionality 
+4. smooth Scroll the page to ensure all elements are tested.
+5. the generated testcase aso check all the pages that contain in that given url and also check all the element functionality like btns
 6. Test in multiple screen sizes (desktop, tablet, mobile).
 7. Print a report with:
-   - PASS/FAIL/ERROR for each step,
+   - PASS/FAIL/ERROR for each elements and step,
    - execution time,
    - overall test score in percentage,
    - summary at the end.
+8. dont use unittest module
+
 
 Output only the full Python code with proper indentation. No explanations.
 """
@@ -26,3 +28,4 @@ Output only the full Python code with proper indentation. No explanations.
     model = genai.GenerativeModel("gemini-1.5-pro")
     response = model.generate_content(prompt)
     return response.text
+
